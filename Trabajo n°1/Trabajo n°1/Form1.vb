fdcc5b6727760f3f783911e10d201b20064a3965
Franb2004 <111138292+Franb2004@users.noreply.github.com>
2022-08-16 11:06:22 -0300

    Add files via upload

---
+++ Trabajo n°1/Trabajo n°1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form2.Show()
+        Me.Top = 100
+        Me.Left = 200
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Trabajo n°1/Trabajo n°1/Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Top = 100
+        Me.Left = 450
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form1.Close()
+    End Sub
+End Class
